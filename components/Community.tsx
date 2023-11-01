6cea3ac47d6efc7f0657f1c2644171be519d518e
Henrique Macedo <henrique@layerx.xyz>
2023-11-01 23:33:37 +0000

    hotfix: update discord link on community section

--- components/Community.tsx
+++ components/Community.tsx
@@ -1,13 +1,13 @@
-import styled from "styled-components";
-import { rem } from "polished";
-import Section from "@/ui/Section";
-import ProductShot from "@/ui/ProductShot";
-import { ButtonLink } from "@taikai/rocket-kit";
-import { useAnalytics } from "@/utils/analytics";
+import styled from 'styled-components';
+import { rem } from 'polished';
+import Section from '@/ui/Section';
+import ProductShot from '@/ui/ProductShot';
+import { ButtonLink } from '@taikai/rocket-kit';
+import { useAnalytics } from '@/utils/analytics';
 
 const CustomProductShot = styled.div`
   img {
-    max-height: ${rem("400px")};
+    max-height: ${rem('400px')};
   }
 `;
 
@@ -25,10 +25,10 @@ const Community = () => {
         >
           <ButtonLink
             color="blue500"
-            url="https://discord.gg/9aUufhzhfm"
+            url="https://discord.gg/layerx"
             value="Join our Discord"
             action={() => {
-              analytics.pushEvent("discord_button");
+              analytics.pushEvent('discord_button');
             }}
             rounded
             blank
