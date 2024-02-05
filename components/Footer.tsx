0fbf232cfcd804005281e722082e2dd7f80e81f3
Henrique Macedo <henrique@layerx.xyz>
2024-02-05 17:41:20 +0000

    docs: update ecosystem links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,133 +1,128 @@
-import Section from "@/ui/Section";
-import FooterMenu from "@/ui/FooterMenu";
+import Section from '@/ui/Section';
+import FooterMenu from '@/ui/FooterMenu';
 
 const Footer = () => {
   const links = [
     {
-      title: "Company",
+      title: 'Company',
       links: [
         {
-          value: "About us",
-          url: "https://layerx.xyz/about",
+          value: 'About us',
+          url: 'https://layerx.xyz/about',
           external: true,
         },
         {
-          value: "Careers",
-          url: "https://layerx.xyz/about#open-positions",
+          value: 'Careers',
+          url: 'https://layerx.xyz/about#open-positions',
           external: true,
         },
         {
-          value: "Blog",
-          url: "https://blog.bepro.network",
+          value: 'Blog',
+          url: 'https://blog.bepro.network',
           external: true,
         },
         {
-          value: "Blog RSS Feed",
-          url: "https://bepro.network/blog/rss",
+          value: 'Blog RSS Feed',
+          url: 'https://bepro.network/blog/rss',
           external: true,
         },
         {
-          value: "Roadmap",
-          url: "https://layerxlabs.notion.site/8c8fef2ea0a14e50bb254ee16a8c15e4?v=98c43e733f04481d83f0e1a075fe24ac",
+          value: 'Roadmap',
+          url: 'https://layerxlabs.notion.site/8c8fef2ea0a14e50bb254ee16a8c15e4?v=98c43e733f04481d83f0e1a075fe24ac',
           external: true,
         },
         {
-          value: "Press Kit",
-          url: "https://layerxlabs.notion.site/Bepro-Network-Community-Press-Kit-4dd9c5f6d968444fb2ffd55d85617a71",
+          value: 'Press Kit',
+          url: 'https://layerxlabs.notion.site/Bepro-Network-Community-Press-Kit-4dd9c5f6d968444fb2ffd55d85617a71',
           external: true,
         },
       ],
     },
     {
-      title: "Ecosystem",
+      title: 'Ecosystem',
       links: [
         {
-          value: "Docs",
-          url: "https://support.bepro.network",
+          value: 'Docs',
+          url: 'https://support.bepro.network',
           external: true,
         },
         {
-          value: "I am a builder",
-          url: "https://support.bepro.network/en/collections/3680434-developers",
+          value: 'I am a builder',
+          url: 'https://blog.bepro.network/creating-a-task-in-the-marketplace',
           external: true,
         },
         {
-          value: "I am a holder",
-          url: "https://support.bepro.network/en/collections/3680436-bepro-holder",
+          value: 'I am a task creator',
+          url: 'https://blog.bepro.network/creating-a-task-in-the-marketplace',
           external: true,
         },
         {
-          value: "Open Grants",
-          url: "https://github.com/bepronetwork/grants-program",
-          external: true,
-        },
-        {
-          value: "Bepro v1",
-          url: "https://development.bepro.network",
+          value: 'I am a holder',
+          url: 'https://blog.bepro.network/how-to-review-and-distribute-rewards-in-the-task-marketplace',
           external: true,
         },
       ],
     },
     {
-      title: "Legal",
+      title: 'Legal',
       links: [
         {
-          value: "Terms and Conditions",
-          url: "/terms",
+          value: 'Terms and Conditions',
+          url: '/terms',
         },
         {
-          value: "Privacy Policy",
-          url: "https://taikai.network/privacy",
+          value: 'Privacy Policy',
+          url: 'https://taikai.network/privacy',
           external: true,
         },
       ],
     },
     {
-      title: "Help",
+      title: 'Help',
       links: [
         {
-          value: "Tokenomics",
-          url: "https://support.bepro.network/en/articles/6664430-bepro-tokenomics-distribution-schedule",
+          value: 'Tokenomics',
+          url: 'https://support.bepro.network/en/articles/6664430-bepro-tokenomics-distribution-schedule',
           external: true,
         },
         {
-          value: "Knowledge Base",
-          url: "http://support.bepro.network",
+          value: 'Knowledge Base',
+          url: 'http://support.bepro.network',
           external: true,
         },
         {
-          value: "Community FAQ",
-          url: "https://support.bepro.network/en/collections/3680438-community",
+          value: 'Community FAQ',
+          url: 'https://support.bepro.network/en/collections/3680438-community',
           external: true,
         },
       ],
     },
     {
-      title: "Social",
+      title: 'Social',
       links: [
         {
-          value: "GitHub",
-          url: "https://github.com/layerx-labs",
+          value: 'GitHub',
+          url: 'https://github.com/layerx-labs',
           external: true,
         },
         {
-          value: "Discord",
-          url: "https://discord.gg/layerx",
+          value: 'Discord',
+          url: 'https://discord.gg/layerx',
           external: true,
         },
         {
-          value: "Twitter",
-          url: "https://twitter.com/bepronet",
+          value: 'Twitter',
+          url: 'https://twitter.com/bepronet',
           external: true,
         },
         {
-          value: "LinkedIn",
-          url: "https://linkedin.com/company/bepronet",
+          value: 'LinkedIn',
+          url: 'https://linkedin.com/company/bepronet',
           external: true,
         },
         {
-          value: "YouTube",
-          url: "https://youtube.com/c/BEPRONetwork",
+          value: 'YouTube',
+          url: 'https://youtube.com/c/BEPRONetwork',
           external: true,
         },
       ],
@@ -138,7 +133,7 @@ const Footer = () => {
     <Section padding="s">
       <FooterMenu
         links={links}
-        copyright={["© ", new Date().getFullYear(), " All rights reserved"]}
+        copyright={['© ', new Date().getFullYear(), ' All rights reserved']}
       />
     </Section>
   );
